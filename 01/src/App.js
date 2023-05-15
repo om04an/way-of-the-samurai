1a1e3eacbe7f15682bf3a1ed3d55362c4d9b3cb2
om04an <mivanpass@gmail.com>
2023-05-15 20:45:55 +0300

    creation of a social network start

--- 01/src/App.js
+++ 01/src/App.js
@@ -1,13 +1,38 @@
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
-
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://www.clipartmax.com/png/full/122-1225327_one-piece-anime-logo-one-piece-logo-png.png' alt='logo' />
+      </header>
+      <nav className='nav'>
+        <div><a href='#s'>Profile</a></div>
+        <div><a href='#s'>Messages</a></div>
+        <div><a href='#s'>News</a></div>
+        <div><a href='#s'>Music</a></div>
+        <div><a href='#s'>Settings</a></div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='https://wallpaperaccess.com/full/328218.jpg' alt='los-angelea' />
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          my post
+          <div>
+            new post
+          </div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
