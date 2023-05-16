0d1d18ee1cc0f76abbcbf2649ae5e4890cbf009f
om04an <mivanpass@gmail.com>
2023-05-16 10:21:14 +0300

    crushing into main components

---
+++ 01/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://www.clipartmax.com/png/full/122-1225327_one-piece-anime-logo-one-piece-logo-png.png' alt='logo' />
+        </header>
+    )
+}
+
+export default Header;
---
+++ 01/src/components/Profile.jsx
@@ -0,0 +1,26 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://wallpaperaccess.com/full/328218.jpg' alt='los-angelea' />
+            </div>
+            <div>
+                ava + description
+            </div>
+                <div>
+                    my post
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
