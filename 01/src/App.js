25b0bf85efa0a2ea4f529d99c73a05ef96cb9093
om04an <mivanpass@gmail.com>
2023-05-17 21:51:18 +0300

    Added transition by components

--- 01/src/App.js
+++ 01/src/App.js
@@ -2,14 +2,29 @@ import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Profile from './components/Profile/Profile';
+import Messages from './components/Messages/Messages';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Nav />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Nav />
+        <div className='app-wrapper-content'>
+          <Routes>
+            <Route path='/messages' element={<Messages/>} />
+            <Route path='/profile' element={<Profile/>}/>
+            <Route path='/news' element={<News/>}/>
+            <Route path='/music' element={<Music/>}/>
+            <Route path='/settings' element={<Settings/>}/>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
